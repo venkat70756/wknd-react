80318d481766a24d7c468574c4c50f942bff1711
TechStackPulse <ourcode10@gmail.com>
2025-05-17 22:44:36 +0530

    sample component creation

---
+++ ui.frontend/src/components/Header/header.js
@@ -0,0 +1,15 @@
+//Header.js
+import React, {Component} from 'react';
+
+export default class Header extends Component {
+
+    render() {
+        return (
+                <header className="Header">
+                    <div className="Header-container">
+                        <h1>Venkat</h1>
+                    </div>
+                </header>
+        );
+    }
+}
